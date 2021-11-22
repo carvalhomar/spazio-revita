5e7467acfbcb96f03629c9626250d7ae3a0cb26c
Marcelo Carvalho de Moura <marcelo@studioti.com.br>
2021-11-22 15:40:42 -0300

    change video thumbs

--- resources/views/page/video.blade.php
+++ resources/views/page/video.blade.php
@@ -20,7 +20,7 @@
             <div class="col-md-6 wow fadeInUp">
                 <div class="card" style="width: 35rem;">
                     <a href="https://youtu.be/BpzoV6Ds7wU" target="_blank">
-                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/bioplastia-gluteos.png')}}" alt="">
+                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/bio-gluteos-ok.png')}}" alt="">
                     </a>
                 </div>
             </div>
@@ -28,7 +28,7 @@
             <div class="col-md-6 wow fadeInUp">
                 <div class="card" style="width: 35rem;">
                     <a href="https://youtu.be/ksZCAwBXw34" target="_blank">
-                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/conhecendo-depilacao-laser.png')}}" alt="">
+                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/dep-luz-laser-ok.png')}}" alt="">
                     </a>
                 </div>
             </div>
@@ -37,19 +37,20 @@
         <div class="row">
             <div class="col-md-6 wow fadeInUp">
                 <div class="card" style="width: 35rem;">
-                    <a href="https://youtu.be/cMqAOWfsXcA" target="_blank">
-                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/preenchimento-rugas2.png')}}" alt="">
+                    <a href="https://youtu.be/X8L5HzSgMCk" target="_blank">
+                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/bio-geral-ok.png')}}" alt="">
                     </a>
                 </div>
+            </div>
+            <div class="col-md-6 wow fadeInUp">
+{{--                <div class="card" style="width: 35rem;">--}}
+{{--                    <a href="https://youtu.be/cMqAOWfsXcA" target="_blank">--}}
+{{--                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/preenchimento-rugas2.png')}}" alt="">--}}
+{{--                    </a>--}}
+{{--                </div>--}}
             </div>
 
-            <div class="col-md-6 wow fadeInUp">
-                <div class="card" style="width: 35rem;">
-                    <a href="https://youtu.be/X8L5HzSgMCk" target="_blank">
-                        <img class="card-img-top" src="{{ URL::asset('assets/images/videos/bioplastia-geral.png')}}" alt="">
-                    </a>
-                </div>
-            </div>
+
         </div>
     </div>
 
